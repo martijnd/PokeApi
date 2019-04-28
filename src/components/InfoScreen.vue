c2e4f721b2cf462ac5054a712b96a22feebdfb73
martijnd <martijn.dorsman@gmail.com>
2019-04-28 18:22:04 +0200

    Added moves

--- src/components/InfoScreen.vue
+++ src/components/InfoScreen.vue
@@ -1,9 +1,15 @@
 <template>
 <div class="info-screen">
-    <span class="name">{{ pokemon.name }}</span>
+    <div class="name">{{ pokemon.name }}</div>
     <div class="wrapper">
-        <span class="height">Height: {{ computedHeight }} cm</span>
-        <span class="weight">Weight: {{ computedWeight }} kg</span>
+        <div class="row">
+            <span class="height">Height: {{ computedHeight }} cm</span>
+            <span class="weight">Weight: {{ computedWeight }} kg</span>
+        </div>
+        <h3>Moves</h3>
+        <div class="move-container" v-for="move in pokemon.moves" :key="move.title">
+            <div class="title">{{ move.move.name }}</div>
+        </div>
     </div>
 </div>
 </template>
@@ -31,21 +37,34 @@ export default class LeftPanel extends Vue {
 $background: #15FD00;
 
 .info-screen {
+    margin-top: 30px;
     background-color: $background;
     width: 250px;
-    height: 100px;
-    position: absolute;
-    top: 200px;
+    height: 270px;
+    overflow-y: scroll;
     border-radius: 10px;
     padding: 10px;
     .name {
         font-size: 20px;
+        margin-bottom: 10px;
         font-weight: 700;
         text-transform: capitalize;
     }
-    .wrapper {
+    .wrapper .row {
         display: flex;
         justify-content: space-evenly;
+    }
+    h3 {
+        display: block;
+    }
+    .move-container {
+        display: block;
+        margin-bottom: 10px;
+        .title {
+            font-weight: 600;
+            font-size: 16px;
+            margin-bottom: 5px;
+        }
     }
 }
 </style>
